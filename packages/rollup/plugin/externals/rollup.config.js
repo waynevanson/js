7771684e3dae96be34fdac5314a160937beed3eb
Wayne Van Son <waynevanson@gmail.com>
2025-03-07 13:25:58 +1100

    more fixes

--- packages/rollup/plugin/externals/rollup.config.js
+++ packages/rollup/plugin/externals/rollup.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "rollup"
 import typescript from "@rollup/plugin-typescript"
-import dts from "rollup-plugin-dts"
 
 export default defineConfig({
   input: "src/index.ts",
   output: { format: "esm", dir: "dist" },
-  plugins: [typescript(), dts()],
+  plugins: [typescript({})],
 })
