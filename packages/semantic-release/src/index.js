0156605548305757fe6c42f6bc9cfc2d642c4369
Wayne Van Son <waynevanson@gmail.com>
2025-03-10 16:45:35 +1100

    fix: remove adding scoep name to release notes

--- packages/semantic-release/src/index.js
+++ packages/semantic-release/src/index.js
@@ -1,6 +1,4 @@
 //@ts-check
-import * as fs from "node:fs"
-import * as path from "node:path"
 
 const packageName =
   process.env["npm_package_name"] || process.env["PNPM_PACKAGE_NAME"]
@@ -13,13 +11,6 @@ if (!packageName) {
 
 const version = "${version}"
 
-// includes scope and package name in the github release for the changelog.
-const headerPartial = fs
-  .readFileSync(path.resolve(import.meta.dirname, "./header.hbs"), {
-    encoding: "utf8",
-  })
-  .replaceAll(/__PACKAGE_NAME__/g, packageName)
-
 export default {
   branches: ["main"],
   tagFormat: `${packageName}@${version}`,
@@ -27,14 +18,7 @@ export default {
   plugins: [
     // todo: add more field commits here
     "@semantic-release/commit-analyzer",
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        writerOpts: {
-          headerPartial,
-        },
-      },
-    ],
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/npm",
       {
