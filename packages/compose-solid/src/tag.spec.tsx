a34976ead0185ce2cd1ef41881191a48f25bb58c
Wayne Van Son <waynevanson@gmail.com>
2025-03-13 14:04:36 +1100

    test(tag): adds element tage names

---
+++ packages/compose-solid/src/tag.spec.tsx
@@ -0,0 +1,22 @@
+import { ComposedComponent, tag, TagKind } from "./index.jsx"
+import { describe, test, expect, expectTypeOf } from "vitest"
+import { DOMElements } from "solid-js/web"
+import { render } from "@solidjs/testing-library"
+
+describe("tag", () => {
+  test("should create a component with the type sof that tag", () => {
+    const Component = tag("a")
+    expectTypeOf(Component).toEqualTypeOf<ComposedComponent<"a", {}>>()
+  })
+
+  const tags = Array.from(DOMElements.values()) as Array<TagKind>
+
+  test.each(tags)(
+    "should render component with the same tag name for %s",
+    (tagName) => {
+      const Component = tag(tagName)
+      const screen = render(() => <Component />)
+      expect(screen.container.children.item(0)?.localName).toBe(tagName)
+    },
+  )
+})
