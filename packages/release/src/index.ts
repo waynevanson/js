dc63619e3af70cc03c7794a11444ab244ac9f9ae
Wayne Van Son <waynevanson@gmail.com>
2025-03-13 15:07:43 +1100

    chore: add some thoughts on a release plugin system

---
+++ packages/release/src/index.ts
@@ -0,0 +1,13 @@
+// imagine a release framework that worked in all ecosystems.
+// What would it look like?
+// how would different plugins targetting different ecosystems looks?
+// goals: publish to any registry, anywhere without restrictions.
+
+// what version?
+/// the plugins have plugins :D
+// releasing it
+
+// 5. How to get the next version?
+// some people use git commits, some people just want to bump it all the time, some people want ot
+// 6. release to a registry
+//  - command that
