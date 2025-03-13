ac62eff87bc6b853043f94548df1a9b27f99eba7
Wayne Van Son <waynevanson@gmail.com>
2025-03-13 13:58:33 +1100

    fix(tag): removes outer types on result

--- packages/compose-solid/src/tag.ts
+++ packages/compose-solid/src/tag.ts
@@ -2,9 +2,7 @@ import { JSX, splitProps } from "solid-js"
 import { createDynamic } from "solid-js/web"
 import type { TagKind, ComposedComponent } from "./types.js"
 
-export function tag<Tag extends TagKind>(
-  tag: Tag,
-): ComposedComponent<Tag, JSX.IntrinsicElements[Tag]> {
+export function tag<Tag extends TagKind>(tag: Tag): ComposedComponent<Tag, {}> {
   return function PolymorphicTagComponent(props: any) {
     const [, rest] = splitProps(props, ["as"])
     return createDynamic(() => props.as || tag, rest)
