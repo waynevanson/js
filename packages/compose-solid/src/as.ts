7e4e521a37b7144aa58ef6519c2f8c4f47d13b72
Wayne Van Son <waynevanson@gmail.com>
2025-05-05 08:34:40 +1000

    perf: remove createMemo for `as` prop

--- packages/compose-solid/src/as.ts
+++ packages/compose-solid/src/as.ts
@@ -1,4 +1,4 @@
-import { createComponent, createMemo, mergeProps, splitProps } from "solid-js"
+import { createComponent, mergeProps, splitProps } from "solid-js"
 import {
   ComposedPolymorphicComponent,
   ComposedPolymorphicProps,
@@ -6,21 +6,37 @@ import {
   TagKind,
 } from "./types.js"
 
+/**
+ * @summary
+ * Creates a new component where the default element tag is changed.
+ * @param component The target component
+ * @param as The tag to use for the component
+ * @returns A new polymorphic component.
+ *
+ * @example
+ * import { tag, as } from "@waynevanson/compose-solid"
+ *
+ * const First = tag('a')
+ * const Second = as(First, 'button')
+ */
 export function as<
-  Tag extends TagKind,
+  TagPrev extends TagKind,
   OuterProps extends PropsKind,
-  As extends TagKind = Tag,
+  As extends TagKind,
 >(
-  component: ComposedPolymorphicComponent<Tag, OuterProps>,
+  component: ComposedPolymorphicComponent<TagPrev, OuterProps>,
   as: As,
 ): ComposedPolymorphicComponent<As, OuterProps> {
   return function ComposedAsComponent(
     props: ComposedPolymorphicProps<As, OuterProps>,
   ) {
     const [, rest] = splitProps(props, ["as"])
-    const tag = createMemo(() => props.as || as)
-    const next = mergeProps(rest, () => ({ as: tag() }))
-    //@ts-expect-error
+    const next = mergeProps(rest, {
+      get as() {
+        return props.as || as
+      },
+    })
+    //@ts-ignore-error
     return createComponent(component, next)
   }
 }
