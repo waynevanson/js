ca07ae78edce8125a3c2bbc600d7f971097cecb2
Wayne Van Son <waynevanson@gmail.com>
2025-05-07 09:37:12 +1000

    docs: adds info to types

--- packages/compose-solid/src/types.ts
+++ packages/compose-solid/src/types.ts
@@ -25,17 +25,28 @@ export interface ComposedMonomorphicComponent<
   (props: ComposedMonomorphicProps<Tag, OuterProps>): JSX.Element
 }
 
+/**
+ * @summary Replaces the props of the element with the new props.
+ */
 export type ComposedMonomorphicProps<
   Tag extends TagKind,
   OuterProps extends OuterPropsKind,
 > = Substitute<JSX.IntrinsicElements[Tag], OuterProps>
 
+/**
+ * @summary
+ * Replaces the keys of left with the keys of right.
+ */
 export type Substitute<Left extends object, Right extends object> = FastOmit<
   Left,
   keyof Right
 > &
   Right
 
+/**
+ * @summary
+ * Just like the built in `Omit` type but faster!
+ */
 export type FastOmit<T extends object, U extends PropertyKey> = {
   [K in keyof T as K extends U ? never : K]: T[K]
 }
