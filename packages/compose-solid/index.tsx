1871ef4b4ad620dfb66669c2c1f6d3bc46b9ebbb
Wayne Van Son <waynevanson@gmail.com>
2025-06-10 09:19:49 +1000

    build: add examples

--- packages/compose-solid/index.tsx
+++ packages/compose-solid/index.tsx
@@ -1,8 +1,27 @@
-import {} from "./src/index.jsx"
+import { tag, required } from "./src/index.jsx"
 import { render } from "solid-js/web"
 
+const Main = tag("main")
+
+const List = tag("ul")
+
+const Item = tag("li")
+
+const Button = required(tag("button"), ["children"])
+
+// for styled api
+// add a class to classList
+
 function App() {
-  return <main>Hello, World!</main>
+  return (
+    <Main>
+      <List>
+        <Item>
+          <Button>Hit me</Button>
+        </Item>
+      </List>
+    </Main>
+  )
 }
 
 render(() => <App />, document.body)
